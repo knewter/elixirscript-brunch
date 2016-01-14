add1ae0b4966b662044809062b6e9381061e4409
Bryan Joseph <bryanjos@gmail.com>
2016-01-13 20:55:25 -0600

    Updated to debounce in order to make sure it doesn't rerun compilation for each file in the beginning

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 "use strict";
 
 var exec = require('child_process').exec;
+var fs = require('fs');
 var temp = require("temp").track();
 var rollup = require('rollup');
 var babel = require('babel-core');
 
 function ElixirScriptPlugin(config) {
-  this.compiled = false;
-  this.config = config.plugins && config.plugins.elixirscript || {};
+  var elixirscriptConfig = config.plugins && config.plugins.elixirscript || {};
+
+  this.config = Object.assign({ convertToES5: true }, elixirscriptConfig);
+
+  if(!this.config.inputFolder){
+    throw new Error("'inputFolder' required");
+  }
+
+  if(!this.config.mainModule){
+    throw new Error("'mainModule' required");
+  }
+
+  if(!this.config.outputFolder){
+    throw new Error("'outputFolder' required");
+  }
+
+  this.config.inputFolder = this.stripTrailingSlash(this.config.inputFolder) + "/**/*.ex*";
+  this.config.outputFolder = this.stripTrailingSlash(this.config.outputFolder);
+
+  this.compile = this.debounce((params, callback) => { this.doCompile(params, callback) }, 250);
 }
 
 // Tell Brunch we are indeed a plugin for it
@@ -19,39 +38,70 @@ ElixirScriptPlugin.prototype.type = "javascript";
 ElixirScriptPlugin.prototype.extension = "ex";
 ElixirScriptPlugin.prototype.pattern = /\.ex(s|js)?/;
 
-// On-the-fly compilation callback (file by file); assumes Brunch already
-// accepted that file for our plugin by checking `type`, `extension` and
-// `pattern`.
-ElixirScriptPlugin.prototype.compile = function(params, callback) {
-  var classScope = this;
+ElixirScriptPlugin.prototype.stripTrailingSlash = function(str){
+  var lastIndex = str.length - 1;
 
-  if(classScope.compiled == false){
-    classScope.compiled = true;
-  }else if(classScope.compiled == true){
-    return callback(null, null);
+  if(str.substr(lastIndex) === '/') {
+      return str.substr(0, lastIndex);
   }
 
-  temp.mkdir('elixirscript-brunch', function(err, dirPath) {
-    if(err){
-      return callback(err);
+  return str;
+}
+
+ElixirScriptPlugin.prototype.debounce = function(func, wait){
+  var timeout;
+  var previousCallback = null;
+	return function(params, callback) {
+
+    var later = function() {
+			timeout = null;
+      previousCallback = null;
+      func(params, callback);
+		};
+
+		clearTimeout(timeout);
+    if(previousCallback){
+      previousCallback(null, null);
     }
 
-    exec("elixirscript '" + classScope.config.path + "' -o '" + dirPath + "'", function(error, stdout, stderr){
+    previousCallback = callback;
+    timeout = setTimeout(later, wait);
+	};
+}
+
+ElixirScriptPlugin.prototype.doCompile = function(params, callback){
+  var inputFolder = this.config.inputFolder;
+  var mainModule = this.config.mainModule;
+  var convertToES5 = this.config.convertToES5;
+  var outputPath = this.config.outputFolder + "/" + mainModule + ".js";
+
+  temp.mkdir('elixirscript-brunch', function(err, dirPath) {
+
+    exec("elixirscript '" + inputFolder + "' -o '" + dirPath + "'", function(error, stdout, stderr){
       if(error){
         return callback(error);
       }
 
       rollup.rollup({
-        entry: dirPath + "/Elixir." + classScope.config.main + ".js"
+        entry: dirPath + "/Elixir." + mainModule + ".js"
       }).then(function (bundle) {
 
         var result = bundle.generate({ format: 'es6' });
-        result = babel.transform(result.code, { presets: ['es2015'] })
-        classScope.compiled = false;
-        return callback(null, result.code);
+
+        if(convertToES5){
+          result = babel.transform(result.code, { presets: ['es2015'] })
+        }
+
+        fs.writeFile(outputPath, result.code, function (err) {
+          if (err){
+            return callback(err);
+          }
+
+          return callback(null, null);
+        });
       });
     });
   });
-};
+}
 
 module.exports = ElixirScriptPlugin;
