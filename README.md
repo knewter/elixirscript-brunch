7e09154b4504cd1ee36cf844c3b6eaeada5dd9c3
Bryan Joseph <bryanjos@gmail.com>
2017-02-26 13:58:02 -0600

    Updated for use with elixirscript:master

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 This plugin for brunch adds support for ElixirScript files.
 
+```bash
+npm install --save elixirscript-brunch
+```
+
 It will look for files with the extensions `ex`, `exs`, or `exjs` in your watched paths
 
 It depends on the `elixirscript` binary being in your path. For more information check here: [elixirscript](https://github.com/elixirscript/elixirscript)
@@ -12,34 +16,22 @@ elixirscript-brunch relies on the following options:
 plugins: {
   ...
   elixirscript: {
-    inputFolder: "web/static/exjs",
-    outputFolder: "web/static/js",
-    format: "es",
-    configPath: "path/to/my/elixirscript.exs"
+    inputFolder: "my/exjs/code/path",
+    outputFolder: "my/output/path",
+    format: "umd",
+    jsModules: [
+      ["React", "react"]
+    ]
   }
 }
 ```
 
-`inputFolder` (required if not specified in a config file): This is the folder that your elixirscript code is located
+`inputFolder` (optional): This is the folder that your elixirscript code is located. 
+Defaults to current working directory
 
-`outputFolder` (required if not specified in a config file): This is the folder that the output will end up in.
+`outputFolder` (optional): This is the folder that the output will end up in.
+Defaults to 'js' folder in paths.public
 
-`format`  (optional): The module format of the output. Defaults to "es"
+`format` (optional): The module format of the output. Defaults to "umd"
 
-`configPath`  (optional): The path to your elixirscript.exs file. Defaults to current directory
-
-Usage with Phoenix:
-  This is just an example of how you could use this plugin with Phoenix
-
-  1. Install the elixirscript-brunch plugin:
-  ```bash
-  npm install --save elixirscript-brunch
-  ```
-
-  *Note*: In your package.json file, be sure to place it above any of your other
-  Brunch plugins. The reason for this is that Brunch runs plugins in the order that
-  they appear in the package.json file.
-
-  2. Configure the elixirscript-brunch plugin as described above.
-  Using the above settings, your elixirscript code will be in the `web/static/exjs` folder
-  and output will end up in your `web/static/js` folder.
+`jsModules` (optional): A list of JavaScript modules used
--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 "use strict";
 
-var exec = require("child_process").exec;
+var childProcess = require("child_process");
+var path = require("path");
 
 function ElixirScriptPlugin(config) {
+  var defaults = {
+    inputFolder: process.cwd(),
+    outputFolder: path.join(config.paths.public, "js"),
+    format: "umd",
+    jsModules: []
+  };
+
   var elixirscriptConfig = config.plugins && config.plugins.elixirscript || {};
-
-  this.config = Object.assign({}, elixirscriptConfig);
-
-  this.config.inputFolder = this.stripTrailingSlash(this.config.inputFolder);
-  this.config.outputFolder = this.stripTrailingSlash(this.config.outputFolder);
-
-  this.compile = this.doCompile;
+  this.config = Object.assign({}, defaults, elixirscriptConfig);
 }
 
 // Tell Brunch we are indeed a plugin for it
@@ -23,67 +25,42 @@ ElixirScriptPlugin.prototype.extension = "ex";
 ElixirScriptPlugin.prototype.pattern = /\.ex(s|js)?/;
 
 ElixirScriptPlugin.prototype.stripTrailingSlash = function(str) {
-  var lastIndex = str.length - 1;
-
-  if (str.substr(lastIndex) === "/") {
-    return str.substr(0, lastIndex);
-  }
-
-  return str;
+  const separatedPath = str.split(path.sep);
+  path.join.apply(path, separatedPath);
 };
 
-ElixirScriptPlugin.prototype.debounce = function(func, wait) {
-  var timeout;
-  var previousCallback = null;
-  return function(params, callback) {
-    var later = function() {
-      timeout = null;
-      previousCallback = null;
-      func(params, callback);
-    };
-
-    clearTimeout(timeout);
-    if (previousCallback) {
-      previousCallback(null, null);
-    }
-
-    previousCallback = callback;
-    timeout = setTimeout(later, wait);
-  };
+ElixirScriptPlugin.prototype.compile = function(file, callback) {
+  return callback(null, "");
 };
 
-ElixirScriptPlugin.prototype.doCompile = function(params, callback) {
+ElixirScriptPlugin.prototype.onCompile = function() {
   var args = ["elixirscript"];
 
   if (this.config.inputFolder) {
-    var inputFolders = Array.isArray(this.config.inputFolder)
-      ? this.config.inputFolder
-      : [this.config.inputFolder];
+    var input = [];
 
-    args = args.concat(inputFolders);
+    if (Array.isArray(this.config.inputFolder)) {
+      input = this.config.inputFolder;
+    } else {
+      input = [this.config.inputFolder];
+    }
+
+    input = input.join(" ");
+
+    args.push(input);
   }
 
-  if (this.config.outputFolder) {
-    args = args.concat(["-o", this.config.outputFolder]);
-  }
+  args = args.concat(["-o", this.config.outputFolder]);
+  args = args.concat(["-f", this.config.format]);
 
-  if (this.config.format) {
-    args = args.concat(["-f", this.config.format]);
-  }
+  modules = this.config.jsModules.map(function(module) {
+    return "--js-module" + module.join(":");
+  });
 
-  if (this.config.configPath) {
-    args = args.concat(["-c", this.config.configPath]);
-  }
+  args = args.concat(modules);
 
   var command = args.join(" ");
-
-  exec(command, function(error, stdout, stderr) {
-    if (error) {
-      return callback(error);
-    } else {
-      return callback(null, null);
-    }
-  });
+  childProcess.execSync(command);
 };
 
 module.exports = ElixirScriptPlugin;
